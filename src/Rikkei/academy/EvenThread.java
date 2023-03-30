9a0b5f24116e76db391494202aaa054d7587a45b
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-03-30 10:51:37 +0700

    3003

---
+++ src/Rikkei/academy/EvenThread.java
@@ -0,0 +1,18 @@
+package Rikkei.academy;
+
+public class EvenThread extends Thread{
+    private Thread t;
+    public EvenThread() {
+    }
+
+    public void run(){
+        for (int i = 2; i < 11; i+=2) {
+                System.out.println(i);
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ src/Rikkei/academy/OddThread.java
@@ -0,0 +1,20 @@
+package Rikkei.academy;
+
+public class OddThread extends Thread {
+    private Thread t;
+
+    public OddThread() {
+    }
+
+    public void run() {
+        for (int i = 1; i < 11; i += 2) {
+            System.out.println(i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
